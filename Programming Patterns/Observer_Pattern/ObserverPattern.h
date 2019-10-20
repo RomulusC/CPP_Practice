878ab6c2df46bbc0cb0b9815602a760e09ca22c3
unknown <rommulluss@protonmail.com>
2019-10-20 20:57:57 +0100

    20/10/19 20:58

--- Programming Patterns/Observer_Pattern/ObserverPattern.h
+++ Programming Patterns/Observer_Pattern/ObserverPattern.h
@@ -11,6 +11,18 @@ File: ObserverPattern.h--------------------*
 #include <vector>
 #include <iostream>
 #include <algorithm>
+class Sync_out
+{
+	std::mutex m_mutex;
+public:
+	void operator<<(std::string sss)
+	{
+		std::scoped_lock lck(m_mutex);
+		std::cout << sss;
+		
+	}
+};
+Sync_out sync_out;
 class Subject;
 class Observer
 {
@@ -23,32 +35,41 @@ public:
 	void SubscriptionComplete(Subject* _s)
 	{
 		m_SubscribedSubjects.emplace_back(_s);
-	}	
+	}
 	virtual void SubjectDestroyed(Subject* _o) = 0;//THIS IS REQUIRED TO NOTIFY SUBJECT OBJECT DELETION. CANNOT BE DONE IN LOCAL DESTRUCTOR BECAUSE CLASS SUBJECT MUST BE FULLY DEFINED
 	~Observer()
 	{
-		std::cout << "Observer Deleted!\n";
+		std::stringstream ss;
+		ss << "OBSERVER DESTRUCTOR: Deleted!\n";
+		sync_out << ss.str();
 
-	}	
+	}
 };
 class Subject
 {
+private:
+	std::mutex mm_mutex;
+
 protected:
 	std::vector<Observer*> m_observerList;
-
 	void UpdateObservers()
 	{
+		std::scoped_lock<std::mutex> lck(mm_mutex);
 		auto it = m_observerList.begin();
 		while (it != m_observerList.end())
 		{
 			(*it)->Update(this);
 			it++;
 		}
-		std::cout << "---------------------------------------------\n";
+		std::stringstream ss;
+		ss << "---------------------------------------------\n";
+		sync_out << ss.str();
 	}
 public:
 	void SubscribeObserver(Observer* _o)
 	{
+		std::scoped_lock<std::mutex> lck(mm_mutex);
+
 		if (std::find(m_observerList.begin(), m_observerList.end(), _o) == m_observerList.end())
 		{
 			m_observerList.emplace_back(_o);
@@ -57,16 +78,21 @@ public:
 	}
 	void UnsubscribeObserver(Observer* _o)
 	{
+		std::scoped_lock<std::mutex> lck(mm_mutex);
+
 		auto it = std::find(m_observerList.begin(), m_observerList.end(), _o);
 		if (it != m_observerList.end())
 		{
 			m_observerList.erase(it);
 		}
-	}	
+	}
 	~Subject()
 	{
+		std::scoped_lock<std::mutex> lck(mm_mutex);
+		std::stringstream ss;
 		for (auto it : m_observerList)
 			it->SubjectDestroyed(this);
-		std::cout << "Subject Deleted!\n";			
+		ss << "SUBJECT DESTRUCTOR: Deleted!\n";
+		sync_out << ss.str();
 	}
 };
--- Programming Patterns/Observer_Pattern/main.cpp
+++ Programming Patterns/Observer_Pattern/main.cpp
@@ -12,33 +12,35 @@ File: main.cpp-----------------------------*
 #include <thread>
 #include <sstream> 
 #include <shared_mutex>
+#include <future>
 
 #include "ObserverPattern.h"
 
 class AtomicTime : public Subject
 {
 private:
-	std::shared_mutex m_mutex2;
+	std::shared_mutex m_mutex;
 	long long m_msDelay; //m_mutex2;
 	std::chrono::time_point<std::chrono::system_clock> start; //m_mutex2;
 
-	std::shared_mutex m_mutex; 
 	bool stop; //m_mutex
 	bool init; //m_mutex
 
 public:
 	AtomicTime(long long delay)
-		:init(false), stop(false), m_msDelay(delay) {}	
-	
+		:init(false), stop(false), m_msDelay(delay), m_mutex(std::shared_mutex()) {}
+	std::shared_mutex& GetMutex()
+	{
+		return this->m_mutex;
+	}
 	std::string ObserGetTimeString()
-	{		
+	{
 		std::stringstream ss;
-		std::shared_lock<std::shared_mutex> lock(m_mutex2);
+		std::shared_lock<std::shared_mutex> sLock(m_mutex);
 		auto in_time_t = std::chrono::system_clock::to_time_t(start);
 		struct tm timeinfo;
 		localtime_s(&timeinfo, &in_time_t);
-		ss << timeinfo .tm_hour<<":"<<timeinfo.tm_min<< ":" << timeinfo.tm_sec<< " ----------------------- Delay:" << std::to_string(m_msDelay)<<"ms";
-		lock.unlock();
+		ss << timeinfo.tm_hour << ":" << timeinfo.tm_min << ":" << timeinfo.tm_sec << " ----------------------- Delay:" << std::to_string(m_msDelay) << "ms";
 		return ss.str();
 	}
 	~AtomicTime()
@@ -51,55 +53,50 @@ public:
 		while (true)
 		{
 			using namespace std::chrono_literals;
+			std::unique_lock<std::shared_mutex> uLock(m_mutex);
 			if (init == false)
 			{
-				std::unique_lock<std::shared_mutex> lock3(m_mutex2);
 				start = std::chrono::system_clock::now();
-				lock3.unlock();
 				init = true;
 			}
-			std::shared_lock<std::shared_mutex> lock1(m_mutex2);
 			auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start);
 
-			if (duration.count() > m_msDelay )
+			if (duration.count() > m_msDelay)
 			{
-				lock1.unlock();
-				std::unique_lock<std::shared_mutex> lock2(m_mutex2);
 				m_msDelay += 50;
 				if (m_msDelay > 1000)
 					m_msDelay = 200;
 				start = std::chrono::system_clock::now();
-				lock2.unlock();
+				uLock.unlock();
 				UpdateObservers();
-			}						
-			
-			std::shared_lock<std::shared_mutex> lock(m_mutex);
+				uLock.lock();
+			}
 			if (stop == true)
 			{
-				lock.unlock();
 				return;
 			}
-			lock.unlock();				
 		}
 	}
 
 	void DeInit()
-	{			
-		std::unique_lock<std::shared_mutex> lock(m_mutex);
+	{
+		std::unique_lock<std::shared_mutex> uLock(m_mutex);
 		stop = true;
-		return;	
+		return;
 	}
 
 };
 
 class LocalClock : public Observer
 {
-private:	
+private:
+	std::mutex m_mutex;
 	static unsigned int count;
 	int thisId;
 
 	virtual void Destruction() override
 	{
+
 		auto it = m_SubscribedSubjects.begin();
 		while (it != m_SubscribedSubjects.end())
 		{
@@ -116,12 +113,17 @@ public:
 	~LocalClock()
 	{
 		Destruction();
-		
+
 	}
 	virtual void Update(Subject* s) override
 	{
-		AtomicTime* target = static_cast<AtomicTime*>(s);	
-		std::cout <<"Observer ID: "<<thisId<<" ----------- Time: "<< target->ObserGetTimeString() << " -------------------------\n";
+		std::scoped_lock<std::mutex> lk(m_mutex);
+		AtomicTime* target = static_cast<AtomicTime*>(s);
+		//--------------------------------------------------------
+		std::stringstream ss;
+
+		ss << "Observer ID: " << thisId << " ----------- Time: " << target->ObserGetTimeString() << " -------------------------\n";
+		sync_out << ss.str();
 	}
 	unsigned int GetID()
 	{
@@ -132,23 +134,25 @@ public:
 		auto it = std::find(m_SubscribedSubjects.begin(), m_SubscribedSubjects.end(), o_);
 		if (it != m_SubscribedSubjects.end())
 			m_SubscribedSubjects.erase(it);
-	}	
+	}
 };
+
 unsigned int LocalClock::count = 0;
 int main()
 {
-	std::cout << "The Observer Pattern.\n" <<
+	std::stringstream ss;
+	ss << "The Observer Pattern.\n" <<
 		"The AtomicTime object (the subject) runs on it's own thread keeping time duration,\nupdates any objects (obserers) that have subscribed to the subject over a period of time.\n" <<
 
-		"Three observers will be subscribed to one subject in this demonstration."<<"\nPress any key to start.\n";
-	std::cin.get();
-
+		"Three observers will be subscribed to one subject in this demonstration." << "\nPress any key to start.\n";
+	sync_out << ss.str();
+	ss.str("");
 
 	AtomicTime* subject = new AtomicTime(200);
 	LocalClock* observer1 = new LocalClock();
 	LocalClock* observer2 = new LocalClock();
 	LocalClock* observer3 = new LocalClock();
-	
+
 
 	subject->SubscribeObserver(static_cast<Observer*>(observer1));
 	subject->SubscribeObserver(static_cast<Observer*>(observer2));
@@ -161,19 +165,27 @@ int main()
 	subject->SubscribeObserver(static_cast<Observer*>(observer3));
 
 
-	std::thread subjectThread(&AtomicTime::Tick, subject);
-	
+	using namespace std::chrono_literals;
 	std::cin.get();
+	//std::thread subjectThread(&AtomicTime::Tick, subject);
+	auto thread = std::async(std::launch::async, &AtomicTime::Tick, subject);
+	std::this_thread::sleep_for(3s);
 	subject->UnsubscribeObserver(observer1);
-	std::cout << "UNSUBSCRIBED: " << observer1->GetID() << "--------------------------------\n";
-	std::cin.get();
-	std::cout << "DELETING: " << observer3->GetID() << "--------------------------------\n";
+	std::cout << "UNSUBSCRIBED: " << observer1->GetID() << " =====================================\n";
+	sync_out << ss.str();
+	ss.str("");
+	std::this_thread::sleep_for(3s);
+	std::cout << "DELETING: " << observer3->GetID() << " =====================================\n";
+	sync_out << ss.str();
+	ss.str("");
+	subject->UnsubscribeObserver(observer3);
 	delete observer3;
-	std::cout << "DONE! "<< "--------------------------------\n";
+	std::cout << "PRESS TO DELETE SUBJECT! " << " =====================================\n";
+	sync_out << ss.str();
+	ss.str("");
 	std::cin.get();
 	subject->DeInit(); //check if initiating DeInit() or delete in either order causes crash
-	std::cout << "DELETING SUBJECT "<< "--------------------------------\n";
 	delete subject;
-	std::cin.get();	
-	subjectThread.join();	
+	std::cout << "DONE! " << " =====================================\n";
+	std::cin.get();
 }
